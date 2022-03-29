a880915dc84d306df23d5fb076f467da2f14ed6f
Eris Suther <eris.suther@gmail.com>
2022-03-29 09:43:36 -0400

    fixed where third number was collected and stored into n2 instead of n3 and added input validation and changed how the average function returned and added a proper way to exit the program

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -2,22 +2,43 @@
 #include <iomanip>
 using namespace std;
 
-float average(int i1, int i2, int i3)
-{
-	return i1 + i2 + i3 / 3.0f;
-}
+float average(int, int, int);
+int validateInteger();
 
 int main()
 {
 	int n1 =0 , n2 = 0, n3 = 0;
 	cout << "This program calculates the average of three numbers." << endl;
-	cout << "First number: "; cin >> n1;
-	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	cout << "First number: "; 
+	n1 = validateInteger();
+	cout << "Second number: "; 
+	n2 = validateInteger();
+	cout << "Third number: ";
+	n3 = validateInteger();
 
 	float a = average(n1, n2, n3);
 
 	cout << setprecision(1) << fixed << "The average is " << a << endl;
 
-	return 0;
+	cout << endl << "press enter to exit the program";
+	cin.ignore(100, '\n');
+	cin.get();
+}
+
+float average(int i1, int i2, int i3)
+{
+	float math = (i1 + i2 + i3) / 3.0f;
+	return math;
+}
+
+int validateInteger()
+{
+	int variable;
+	while (!(cin >> variable) || cin.fail())
+	{
+		cout << "whoops try again" << endl;
+		cin.clear();
+		cin.ignore(100, '\n');
+	}
+	return(variable);
 }
